dc078be0c7069ae38c0647a004e73fd539a28edd
anaesplicido <163546899+anaesplicido@users.noreply.github.com>
2024-08-30 19:30:38 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuHambButton = document.querySelector(".menu-hamb-button")
-const navBar = document.querySelector(".navbar")
-
-menuHambButton.addEventListener("click", () => {
-    navBar.classList.toggle("show")
-    menuHambButton.classList.toggle("isOpen")
-})
